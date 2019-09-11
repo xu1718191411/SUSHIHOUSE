8bacbb8c3f27f8948cafc733f70be05bb8e95a20
XUZHONGWEI <sjo@curucuru.jp>
2019-09-11 18:05:51 +0900

    add functions

--- back_implement_read_data/functions.py
+++ back_implement_read_data/functions.py
@@ -4,7 +4,7 @@ def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
 def softmax(x):
-    if(x.dim == 1):
+    if(x.ndim == 1):
         max = np.max(x)
         x = x - max
         return np.exp(x) / np.sum(np.exp(x))
@@ -15,3 +15,9 @@ def softmax(x):
         res = np.exp(x) / np.sum(np.exp(x),axis=0)
         result = res.T
         return result
+
+def cross_entropy_error(x,t):
+    delta = 1e-4
+    return np.sum(t * -1 * np.log(x + delta)) / x.shape[0]
+
+
--- back_implement_read_data/main.py
+++ back_implement_read_data/main.py
@@ -1,5 +1,13 @@
 from back_implement_read_data.init_data import init_data
+from back_implement_read_data.functions import *
+
 
 trainImg, trainLabel, testImg, testLabel = init_data()
 
+arr = np.random.randn(12,10)
+
+res = sigmoid(arr)
+result = softmax(res)
 print(1)
+
+
