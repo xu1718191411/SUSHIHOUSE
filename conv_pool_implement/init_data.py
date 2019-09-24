307b725bc3cd36ef9ea41f58167d48f18b6e2139
XUZHONGWEI <sjo@curucuru.jp>
2019-09-24 15:59:01 +0900

    data to one hot

--- conv_pool_implement/init_data.py
+++ conv_pool_implement/init_data.py
@@ -7,10 +7,19 @@ def init_data():
     with open(path,'rb') as f:
         data = pickle.load(f)
         trainImg = formart_to_multi_dimensions(data['train_img'])
+
         trainLabel = data['train_label']
+        data_to_one_hot(trainLabel)
         testImg = formart_to_multi_dimensions(data['test_img'])
         testLabel = data['test_label']
         return trainImg,trainLabel,testImg,testLabel
 
 def formart_to_multi_dimensions(x):
     return np.reshape(x,[-1,1,28,28])
+
+
+def data_to_one_hot(x):
+    result = np.zeros([x.shape[0],10])
+    for index,value in enumerate(x):
+        result[index][value] = 1
+    return result
