efee5383cd9899328807e0b7cbb687a8978dfa29
XUZHONGWEI <sjo@curucuru.jp>
2019-09-24 14:52:46 +0900

    modify

--- conv_pool_implement/init_data.py
+++ conv_pool_implement/init_data.py
@@ -1,16 +1,16 @@
 import os
 import pickle
 import numpy as np
+
 def init_data():
     path = os.path.dirname(os.path.dirname(__file__)) + "/data/mnist.pkl";
     with open(path,'rb') as f:
         data = pickle.load(f)
-        print(1)
-        trainImg = formart(data['train_img'])
-        trainLabel = formart(data['train_label'])
-        testImg = formart(data['test_img'])
-        testLabel = formart(data['test_label'])
+        trainImg = formart_to_multi_dimensions(data['train_img'])
+        trainLabel = data['train_label']
+        testImg = formart_to_multi_dimensions(data['test_img'])
+        testLabel = data['test_label']
         return trainImg,trainLabel,testImg,testLabel
 
-def formart(x):
+def formart_to_multi_dimensions(x):
     return np.reshape(x,[-1,1,28,28])
